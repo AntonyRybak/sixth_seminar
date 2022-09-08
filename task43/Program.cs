c7f2a8f280b5885437f4f74dd26cd23a6851ce65
Anton Rybak <toxa-wow@mail.ru>
2022-09-09 01:37:26 +0300

    task43 done

---
+++ task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значение b1 ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1 ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2 ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2 ");
+double k2 = double.Parse(Console.ReadLine());
+double x = (b2 - b1) / (k1-k2);
+double y = k1 * (b2 - b1) / (k1-k2) + (b1);
+Console.WriteLine($"{x}, {y}");
